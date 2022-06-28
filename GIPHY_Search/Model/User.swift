40b088de5c5fad713a49f41eb72667e821d92247
SJ <forestsj30@gmail.com>
2022-06-29 00:57:17 +0900

    feat: User Model 구현

---
+++ GIPHY_Search/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  GIPHY_Search
+//
+//  Created by Lee Seung-Jae on 2022/06/29.
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    let displayedName: String
+    let description: String
+    let avatarImageURL: String
+}

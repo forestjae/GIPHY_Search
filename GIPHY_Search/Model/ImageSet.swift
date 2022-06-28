fde7ea2a957e9497a5c83c49ce6858ff5448be20
SJ <forestsj30@gmail.com>
2022-06-29 00:57:09 +0900

    feat: ImageSet Model 구현

---
+++ GIPHY_Search/Model/ImageSet.swift
@@ -0,0 +1,13 @@
+//
+//  ImageSet.swift
+//  GIPHY_Search
+//
+//  Created by Lee Seung-Jae on 2022/06/29.
+//
+
+import Foundation
+
+struct ImageSet {
+    let imageURL: String
+    let gridImageURL: String
+}

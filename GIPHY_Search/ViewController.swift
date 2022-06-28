2fc303d9ffe2f1cee62c651241e8efe009d9dbd8
SJ <forestsj30@gmail.com>
2022-06-28 22:22:29 +0900

    feat: 프로젝트 생성 및 타겟 iOS 설정
    - iOS 13

---
+++ GIPHY_Search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GIPHY_Search
+//
+//  Created by Lee Seung-Jae on 2022/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

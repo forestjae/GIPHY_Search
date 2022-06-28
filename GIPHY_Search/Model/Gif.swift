b922cc3d2a18ebab13529570424614a0c1f4fc4e
SJ <forestsj30@gmail.com>
2022-06-29 00:57:26 +0900

    feat: Gif Model 구현

---
+++ GIPHY_Search/Model/Gif.swift
@@ -0,0 +1,15 @@
+//
+//  Gif.swift
+//  GIPHY_Search
+//
+//  Created by Lee Seung-Jae on 2022/06/29.
+//
+
+import Foundation
+
+struct Gif {
+    let identifier: String
+    let title: String
+    let user: User
+    let imageSet: ImageSet
+}

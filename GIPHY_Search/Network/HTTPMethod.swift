a99f3ffe1001778b7175795ce44f7630bc46a56e
SJ <forestsj30@gmail.com>
2022-06-29 04:44:29 +0900

    feat: HTTPMethod enum 구현

---
+++ GIPHY_Search/Network/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  GIPHY_Search
+//
+//  Created by Lee Seung-Jae on 2022/06/29.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}

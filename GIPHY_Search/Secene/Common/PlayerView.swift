3999a1451dbf49fb47fe6d47eff9927e19bb840d
SJ <forestsj30@gmail.com>
2022-06-29 23:35:32 +0900

    feat: Mp4 재생을 위한 PlayerView 커스텀 뷰 구현

---
+++ GIPHY_Search/Secene/Common/PlayerView.swift
@@ -0,0 +1,36 @@
+//
+//  PlayerView.swift
+//  GIPHY_Search
+//
+//  Created by Lee Seung-Jae on 2022/06/29.
+//
+
+import Foundation
+import UIKit
+import AVKit
+import AVFoundation
+
+class PlayerView: UIView {
+
+    override class var layerClass: AnyClass {
+        return AVPlayerLayer.self
+    }
+
+    var playerLayer: AVPlayerLayer? {
+        guard let layer = self.layer as? AVPlayerLayer else {
+            return nil
+        }
+        layer.videoGravity = .resizeAspectFill
+        return layer
+    }
+
+    var player: AVPlayer? {
+        get {
+            return playerLayer?.player
+        }
+
+        set {
+            playerLayer?.player = newValue
+        }
+    }
+}
